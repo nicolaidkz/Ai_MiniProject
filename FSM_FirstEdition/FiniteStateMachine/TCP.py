58dee709e3ef9724d4375e04038b903f6e926920
Nicolai <ndanie16@student.aau.dk>
2020-05-16 23:18:21 +0200

    added client functionality

--- FSM_FirstEdition/FiniteStateMachine/TCP.py
+++ FSM_FirstEdition/FiniteStateMachine/TCP.py
@@ -1,7 +1,11 @@
 import socket
 import threading
+from random import randint
+from time import perf_counter
 import pickle
 import json
+
+serverPost: str = "null"
 
 # this is the ip we are connecting to
 bind_ip = "127.0.0.1"
@@ -18,7 +22,207 @@ server.listen(5)
 print("[+] listening on %s:%d" % (bind_ip, bind_port))
 
 
+##=========================================
+## TRANSITIONS
+
+class Transition(object):
+    def __init__(self, toState):
+        self.toState = toState
+
+
+    def Execute(self):
+        print("Transitioning...")
+
+
+##=========================================
+## STATES
+
+class State(object):
+    def __init__(self, FSM):
+        self.FSM = FSM
+        self.timer = 0
+        self.startTime = 0
+
+    def Enter(self):
+        self.timer = randint(0,5)
+        self.startTime = int(perf_counter())
+    def Execute(self):
+        pass
+    def Exit(self):
+        pass
+
+class Idle(State):
+    def __init__(self, FSM, name):
+        self.name = name
+        super(Idle,self).__init__(FSM)
+
+    def Enter(self):
+        print(self.name + " is starting to become idle.")
+        super(Idle, self).Enter()
+
+    def Execute(self):
+        print(self.name + " is idle.")
+        if (serverPost=="CollectWood"):
+            self.FSM.ToTransition("toCollectWood")
+        if (serverPost=="CollectIron"):
+            self.FSM.ToTransition("toCollectIron")
+
+    def Exit(self):
+        print(self.name + " is no longer idle.")
+
+class CollectWood(State):
+    def __init__(self, FSM, name):
+        self.name = name
+        super(CollectWood,self).__init__(FSM)
+
+    def Enter(self):
+        print(self.name + " has found some wood.")
+        super(CollectWood, self).Enter()
+
+    def Execute(self):
+        print(self.name + " is carrying the wood to the lumbermill.")
+        self.FSM.ToTransition("toLumbermill")
+
+    def Exit(self):
+        print(self.name + " Dropped off the wood at the lumbermill.")
+
+class CollectIron(State):
+    def __init__(self, FSM, name):
+        self.name = name
+        super(CollectIron,self).__init__(FSM)
+
+    def Enter(self):
+        print(self.name + " has found some iron.")
+        super(CollectIron, self).Enter()
+
+    def Execute(self):
+        print(self.name + " is carrying the iron to the blacksmith.")
+        self.FSM.ToTransition("toBlacksmith")
+
+    def Exit(self):
+        print(self.name + " Dropped off the iron at the blacksmith.")
+
+class Lumbermill(State):
+    def __init__(self, FSM, name):
+        self.name = name
+        super(Lumbermill,self).__init__(FSM)
+
+    def Enter(self):
+        print("Lumbermill has received wood from " + self.name + " and is starting to process it")
+        super(Lumbermill, self).Enter()
+
+    def Execute(self):
+        print("Lumbermill is processing the wood.")
+        self.FSM.ToTransition("toIdle")
+
+    def Exit(self):
+        print("Lumbermill has finished processing the wood.")
+
+class Blacksmith(State):
+    def __init__(self, FSM, name):
+        self.name = name
+        super(Blacksmith,self).__init__(FSM)
+
+    def Enter(self):
+        print("Blacksmith has received iron from " + self.name + " and is starting to process it")
+        super(Blacksmith, self).Enter()
+
+    def Execute(self):
+        print("Lumbermill is processing the iron.")
+        self.FSM.ToTransition("toIdle")
+
+    def Exit(self):
+        print("Lumbermill has finished processing the iron.")
+
+
+##=========================================
+## FINITE STATE MACHINES
+
+class FSM(object):
+    def __init__(self, character, name):
+        self.name = name
+        self.char = character
+        self.states = {}
+        self.transitions = {}
+        self.curState = None
+        self.prevState = None
+        self.trans = None
+        #print(1)
+
+    def AddTransition(self, transName, transition):
+        self.transitions[transName] = transition
+        #print(2)
+
+    def AddState(self, stateName, state):
+        self.states[stateName] = state
+        #print(3)
+
+    def SetState(self, stateName):
+        self.prevState = self.curState
+        self.curState = self.states[stateName]
+        #print(4)
+
+    def ToTransition(self, toTrans):
+        self.trans = self.transitions[toTrans]
+        #print(5)
+
+    def Execute(self):
+        #print(6)
+        if(self.trans):
+            self.curState.Exit()
+            self.trans.Execute()
+            self.SetState(self.trans.toState)
+            self.curState.Enter()
+            self.trans = None
+        self.curState.Execute()
+
+
+##=========================================
+## IMPLEMENTATION
+
+Char = type("Char", (object,), {"day":0})
+
+class RobotMaid(Char):
+    def __init__(self, name):
+        self.name = name
+        self.FSM = FSM(self, name)
+
+        ## TRANSITIONS
+        self.FSM.AddTransition("toIdle", Transition("Idle"))
+        self.FSM.AddTransition("toLumbermill", Transition("Lumbermill"))
+        self.FSM.AddTransition("toBlacksmith", Transition("Blacksmith"))
+        self.FSM.AddTransition("toCollectWood", Transition("CollectWood"))
+        self.FSM.AddTransition("toCollectIron", Transition("CollectIron"))
+
+        ## STATES
+        self.FSM.AddState("Idle", Idle(self.FSM, name))
+        self.FSM.AddState("CollectWood", CollectWood(self.FSM, name))
+        self.FSM.AddState("CollectIron", CollectIron(self.FSM, name))
+        self.FSM.AddState("Lumbermill", Lumbermill(self.FSM, name))
+        self.FSM.AddState("Blacksmith", Blacksmith(self.FSM, name))
+
+        self.FSM.SetState("Idle")
+
+    def Execute(self):
+        self.FSM.Execute()
+
+
+if __name__ == '__main__':
+    r = RobotMaid("Claus")
+    #s = RobotMaid("Hendrik")
+    #t = RobotMaid("Rob")
+    #u = RobotMaid("Charlie")
+    #v = RobotMaid("Roger")
+
+def ServerPost(data):
+    serverPost = data
+
+    return serverPost
+
+
 def handle_client(client_socket):
+
+    global serverPost
 
     while True:
 
@@ -27,8 +231,34 @@ def handle_client(client_socket):
 
                 if not data: break
 
-                if data.decode("utf-8") == "TOWER":
-                    print("client says: " + data.decode("utf-8"))
+                if data.decode("utf-8") == "CollectWood":
+                    print("client received: " + data.decode("utf-8") + " from server.")
+                    serverPost = data.decode("utf-8")
+                    for i in range(4):
+                        startTime = perf_counter()
+                        timeInterval = 1
+                        while (startTime + timeInterval > perf_counter()):
+                            pass
+                        r.Execute()
+                        if (i > 2):
+                            serverPost = ""
+                            print("String is " + serverPost + "EMPTY!")
+
+
+                if data.decode("utf-8") == "CollectIron":
+                    print("client received: " + data.decode("utf-8") + " from server.")
+                    serverPost = data.decode("utf-8")
+                    print(serverPost)
+                    for i in range(4):
+                        startTime = perf_counter()
+                        timeInterval = 1
+                        while (startTime + timeInterval > perf_counter()):
+                            pass
+                        r.Execute()
+                        if (i > 2):
+                            serverPost = ""
+                            print("String is " + serverPost + "EMPTY!")
+
 
                 # if data.decode("utf-8") == "GRID":
                 #     print("client says:" + data.decode("utf-8"))
